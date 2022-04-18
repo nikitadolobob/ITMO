0880c0d54578761df0d7166ff0379d5b4203ced2
nikitadolobob <97163742+nikitadolobob@users.noreply.github.com>
2022-04-19 01:37:21 +0300

    Add files via upload

--- lab5/Main.java
+++ lab5/Main.java
@@ -5,14 +5,22 @@ import lab5.parsers.YamlReader;
 
 import java.util.Hashtable;
 
+/**
+ * Класс Main.
+ */
 public class Main {
 
     private static final Hashtable<Integer,Organization> organizations = new Hashtable<>();
 
+    /**
+     * Функция, запускающая приложение.
+     *
+     * @param arg аргументы, которые программа получает при запуске.
+     */
     public static void main(String[] arg){
 
         //System.out.println(System.getenv("PATH"));
-        String filename = ""; //TODO переменная окружения
+        String filename = "C:\\Users\\myav\\IdeaProjects\\ITMO_labs\\src\\lab5\\resources\\zhest.json"; //TODO переменная окружения
 
         YamlReader inputFileReader = new YamlReader(organizations, filename);
         inputFileReader.readData();
